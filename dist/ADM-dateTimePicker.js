56c5938f79e24b3c94eec4d96742836dd4ce8832
Stuart Morgan <smorgan@mythtv.org>
2017-08-21 17:03:17 +0100

    Update ADM-dateTimePicker.js

--- dist/ADM-dateTimePicker.js
+++ dist/ADM-dateTimePicker.js
@@ -938,12 +938,14 @@
                     admDtp.fillDays(_standValue, !scope.option.transition);
                     
                     $timeout(function() {
-                        for (var i = 0; i < scope.current.days.length; i++) {
-                            if (scope.current.days[i].today) {
-                                scope.selectThisDay(scope.current.days[i]);
-                            break;
+                        $scope.$applyAsync(function() {
+                            for (var i = 0; i < scope.current.days.length; i++) {
+                                if (scope.current.days[i].today) {
+                                    scope.selectThisDay(scope.current.days[i]);
+                                break;
+                                }
                             }
-                        }
+                        });
                     },0);
                     
                 }
